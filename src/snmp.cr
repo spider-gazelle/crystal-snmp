44c1336ad94ddd4db2287ceaaa2c8ccf9c4b2cd6
Stephen von Takach <steve@acaprojects.com>
2019-03-15 12:08:59 +1100

    add helpers for accessing various values

--- src/snmp.cr
+++ src/snmp.cr
@@ -30,6 +30,33 @@ class SNMP
     EnterpriseSpecific
   end
 
+  enum ErrorStatus
+    NoError
+    TooBig
+    NoSuchName
+    BadValue
+    ReadOnly
+    GenErr
+  end
+
+  # Custom SNMP tags on varbinds
+  enum AppTags
+    IPAddress
+    Counter32
+    Gauge32 # unsigned integer
+    TimeTicks
+    Opaque
+    # missing 5
+    Counter64 = 6
+  end
+
+  # Error context tags on varbinds
+  enum ContextTags
+    NoSuchObject
+    NoSuchInstance
+    EndOfMibView
+  end
+
   def initialize(ber : ASN1::BER)
     snmp = ber.children
     @version = Version.from_value(snmp[0].get_integer)
@@ -45,10 +72,31 @@ class SNMP
     end
   end
 
+  def initialize(@version, @community, @request, request_id = 0, error_status = ErrorStatus::NoError, error_index = 0)
+    @pdu = PDU.new(request_id, error_status, error_index)
+  end
+
   getter pdu : PDU | TrapPDU
   property version : Version
   property request : Request
   property community : String
+
+  def varbinds
+    @pdu.varbinds
+  end
+
+  def trap?
+    {Request::V1_Trap, Request::V2_Trap, Request::Inform}.includes? @request
+  end
+
+  def expects_response?
+    {Request::GetRequest, Request::GetNext, Request::SetRequest, Request::GetBulk, Request::Inform}.includes? @request
+  end
+
+  def build_reply
+    self.class.new(@version, @community, Request::Response, @pdu.request_id)
+  end
 end
 
 require "./snmp/pdu"
+require "./snmp/v1_trap"
---
+++ src/snmp/v1_trap.cr
@@ -0,0 +1,28 @@
+class SNMP
+  class TrapPDU
+    def initialize(pdu : Array(ASN1::BER))
+      @oid = pdu[0].get_object_id
+      @agent_address = pdu[1].payload.join(".")
+      @generic_trap = GenericTrap.from_value(pdu[2].get_integer)
+      @specific_trap = pdu[3].get_integer.to_i32
+      # timestamp = pdu[4] # TimeTicks
+      @varbinds = pdu[5].children
+    end
+
+    property oid : String
+    property agent_address : String
+    property generic_trap : GenericTrap
+    property specific_trap : Int32
+    @varbinds : Array(ASN1::BER)
+
+    def varbinds
+      @varbinds.map do |varbind|
+        children = varbind.children
+        {
+          oid: children[0].get_object_id,
+          value: children[1]
+        }
+      end
+    end
+  end
+end
