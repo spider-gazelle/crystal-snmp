e8b4bf1ef225736d4a3aaf2b773f71098deb9afb
Stephen von Takach <steve@acaprojects.com>
2019-04-26 14:46:29 +1000

    rename message_id to id as message is redundant

--- src/snmp/v3/message.cr
+++ src/snmp/v3/message.cr
@@ -13,12 +13,15 @@ class SNMP::V3::Message < SNMP::Message
   def initialize(snmp : Array(ASN1::BER))
     # reference: http://www.tcpipguide.com/free/t_SNMPVersion3SNMPv3MessageFormat.htm
     @version = Version.from_value(snmp[0].get_integer)
-    @message_id = snmp[1].get_integer.to_i
+    @id = snmp[1].get_integer.to_i
     @max_size = snmp[2].get_integer.to_i
     @flags = MessageFlags.new(snmp[3].get_bytes[0].to_i)
+
     # TODO:: security model needs an enum:
     @security_model = snmp[4].get_integer.to_i
     @security_params = snmp[5..-2]
+
+    # TODO:: This data is encrypted
     @scoped_pdu = ScopedPDU.new(snmp[-1])
 
     # For compatibility with SNMPv2 Message class
@@ -32,8 +35,7 @@ class SNMP::V3::Message < SNMP::Message
     #@pdu = PDU.new(request_id, error_status, error_index)
   #end
 
-  #getter pdu : PDU | Trap | V1Trap
-  property message_id : Int32
+  property id : Int32
   property max_size : Int32
   property flags : MessageFlags
   property security_model : Int32
